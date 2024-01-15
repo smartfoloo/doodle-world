d7d5c220788d8af808ab51406aa0ec8f842768f4
platformerdotio <smartfoloo@gmail.com>
2024-01-15 11:22:41 -0600

    nn

--- js/site-lock.js
+++
@@ -1,3 +0,0 @@
-if (window.self !== window.top || (window.location.href.indexOf('https://sites.google.com/view/doodle-world-games/games') === -1 && window.location.href.indexOf('http://127.0.0.1:3000/') === -1)) {
-  window.location.href = 'https://helloskids.pages.dev';
-}
